8b775059ddd2a3910f80c6eaf4c449b4249e634f
eihwazzz <mike4hunter@hotmail.com>
2025-01-23 22:28:46 -0300

    Added info card on layout - part 1

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatDividerModule, MatButtonModule],
+  imports: [MatDividerModule, MatButtonModule, MatCardModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  profession:string = "Fullstack Developer";
+  description:string = "I'm continuously seeking to expand my knowledge, as well as my soft and hard skills";
+  email:string = "Martinbrito1985@gmail.com";
 
 }
