20bcaa8673dc7b57dd661504b196586ee6a58616
eihwazzz <mike4hunter@hotmail.com>
2025-01-26 02:57:49 -0300

    Added some efects and fixed on styles and codee

--- src/app/pages/card-game/card-game.component.ts
+++ src/app/pages/card-game/card-game.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 import { MatCardModule } from '@angular/material/card'; // Si usas Angular Material
 
 @Component({
@@ -10,13 +10,35 @@ import { MatCardModule } from '@angular/material/card'; // Si usas Angular Mater
   styleUrl: './card-game.component.css',
   animations: [
     trigger('girar', [
+      state('frente', style({
+        transform: 'rotateY(0deg)', // Estado frontal (mostrando el contenido)
+      })),
+      state('reverso', style({
+        transform: 'rotateY(180deg)', // Estado invertido (reverso de la carta)
+      })),
+      transition('frente <=> reverso', [
+        animate('1s ease-in-out')
+      ])
+    ]),
+    trigger('entrada', [
       transition(':enter', [
-        style({ transform: 'translateX(-100%) rotateY(90deg)', opacity: 0 }),
-        animate('1s 0.3s ease-in-out', style({ transform: 'translateX(0) rotateY(0)', opacity: 1 }))
+        style({
+          transform: 'translateX(-100%) rotateY(180deg)', // Empuja desde la izquierda y gira en 3D
+          opacity: 0
+        }),
+        animate('1s ease-in-out', style({
+          transform: 'translateX(0) rotateY(180deg)', // Lleva al centro y mantiene la rotación de 180 grados
+          opacity: 1
+        }))
       ])
     ])
   ]
 })
 export class CardGameComponent {
   cards = [1, 2, 3];
+  cartaEstado: { [key: number]: 'frente' | 'reverso' } = { 1: 'reverso', 2: 'reverso', 3: 'reverso' };
+
+  girarCarta(cartaId: number): void {
+    this.cartaEstado[cartaId] = this.cartaEstado[cartaId] === 'reverso' ? 'frente' : 'reverso';
+  }
 }
